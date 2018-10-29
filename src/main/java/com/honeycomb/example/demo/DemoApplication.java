cde7d74bb46117a528b68881d4158b2c72e6bfe8
maoliang <maoliang@renrenche.com>
2018-10-29 19:35:34 +0800

    feature:新增web配置

--- src/main/java/com/honeycomb/example/demo/DemoApplication.java
+++ src/main/java/com/honeycomb/example/demo/DemoApplication.java
@@ -1,10 +1,19 @@
 package com.honeycomb.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class DemoApplication {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
